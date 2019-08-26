aa394758887b7440d2d514f7d3561adc72376601
Jason Rowland <jasonrowland@Jasons-MBP.attlocal.net>
2019-08-26 12:26:58 -0500

    flex: 1

--- client/build/precache-manifest.30f6290dfeab5e1031ff5353556176e0.js
+++ client/build/precache-manifest.30f6290dfeab5e1031ff5353556176e0.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a4e69e59299ca90e826b00360fade70b",
+    "revision": "e39531860c7b48869f632a3780f1208b",
     "url": "/index.html"
   },
   {
-    "revision": "51d8ae73a1787b8e7813",
-    "url": "/static/css/main.18ae37c5.chunk.css"
+    "revision": "5a84b99a424eb6c9c96d",
+    "url": "/static/css/main.93e5b3c7.chunk.css"
   },
   {
     "revision": "a8c14b75b93280fff5d8",
     "url": "/static/js/2.7f5eea55.chunk.js"
   },
   {
-    "revision": "51d8ae73a1787b8e7813",
+    "revision": "5a84b99a424eb6c9c96d",
     "url": "/static/js/main.03b8cd62.chunk.js"
   },
   {
